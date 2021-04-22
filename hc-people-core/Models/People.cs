42448fa8dc06c3430fb31f07944e5ec7085dc231
marshallprotsman <47481879+MarshallProtsman@users.noreply.github.com>
2021-04-22 09:58:44 -0400

    begin EFCore implement people Model built react app running w/o data

---
+++ hc-people-core/Models/People.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace hc_people_core.Models
+{
+    public class People
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string name { get; set; }
+
+        public string address { get; set; }
+
+        public int age { get; set; }
+
+        public string interests { get; set; }
+
+        public string avatar { get; set; }
+
+    }
+
+}
