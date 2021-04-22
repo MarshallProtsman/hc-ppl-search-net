eb499ab7c0659774c92970c9c511fe3bc9097ca2
marshallprotsman <47481879+MarshallProtsman@users.noreply.github.com>
2021-04-21 21:02:03 -0400

    set up basic Client and server

---
+++ README.md
@@ -0,0 +1,1 @@
+# hc-ppl-search-net
---
+++ api/hc-people-core/hc-people-core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace hc_people_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
